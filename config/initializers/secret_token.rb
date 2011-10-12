e0e6925df621586e65edac21dbb8d6cc94bca8e2
Seamus Roche <seamusjr@gmail.com>
2011-10-11 23:22:30 -0500

    Init commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RccApp::Application.config.secret_token = '05068f1328b13f7daa1d4ccf69411784834ab351e7bd0813f51074e4cc48647250e7c5be15a967321ac273cfccf6f3294eb0e033687fce5fab8a522e07a88594'
